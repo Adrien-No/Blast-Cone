d270d1b85727d6f8bb5a3b48be0723cabeb2777d
AdriFirst <75485440+AdriFirst@users.noreply.github.com>
2020-12-11 17:39:07 +0000

    Add files via upload

--- Blast-Cone-main/main.py
+++ Blast-Cone-main/main.py
@@ -1,12 +1,12 @@
 import csv, aiohttp, json, asyncio, discord
 from discord.ext import commands
 
-api_key_riot = 'RGAPI-decc5fb8-d2d6-4ee8-9d02-15023e78263d'
-api_key_discord = 'NzQ5MzgyNDc2OTEyMjYzMjg5.X0rK0Q.sAtArrBOa6W3Hql3DSWTnY5MaPQ'
-
-all_my_dicts = {}
-#await dict_build('https://ddragon.leagueoflegends.com/api/versions.json','versions')
-#await ctx.send(all_my_dicts['versions'][0])
+api_key_riot = 'RGAPI-52541b27-ec68-4ff8-afaf-340fb85352bb'
+api_key_discord = 'NzQ5MzgyNDc2OTEyMjYzMjg5.X0rK0Q.P7LoKQrS-GH1p1erfwpLA7gikpo'
+api_version_riot = 'v4'
+discord_id_adrifirst = 346632637143842817
+#on ajoute le dictionnaire qui permet de relié une donnée de région entrée par l'utilisateur à la véritable syntaxe de la région
+all_my_dicts = {'region': {"br":"br1", "eun":"eun1", "euw":"euw1", "jp" :"jp1", "kr":"kr", "la1":"la1", "la2":"la2", "na":"na1", "oc":"oc1","tr":"tr1","ru":"ru" } }
 
 #on récupère les données du fichier csv
 statut_guild_channel = {}
@@ -45,13 +45,13 @@ async def on_ready():
     #envoi une alerte de connexion à tout les serveurs
     for key,value in statut_guild_channel.items():
         channel=bot.get_channel(int(value))
-        await channel.send('**Connected !    :green_circle:**')
+        await channel.send('**:green_circle:   Connected !     :green_circle:**')
     
 @bot.command()
 async def stop(ctx):
     global statut_guild_channel
     
-    if ctx.message.author.id == 346632637143842817:
+    if ctx.message.author.id == discord_id_adrifirst:
         
         #on update le fichier csv avant de quitter
         with open('donnees/statut_channel.csv', 'w') as file:
@@ -64,8 +64,11 @@ async def stop(ctx):
         #envoi une alerte de déconnexion à tout les serveurs
         for key,value in statut_guild_channel.items():
             channel=bot.get_channel(int(value))
-            await channel.send('**Disconnected ! :red_circle:**')
+            await channel.send('*le tout-puissant AdriFirst a ordonné la déconnection du bot...*\n**:red_circle: Disconnected ! :red_circle:**')
+            await ctx.message.delete()
         await bot.logout()
+    else:
+        await ctx.send(f'{ctx.message.author.mention} eh fréro t\'as pas de droit de faire ça !')
 
 @bot.command()
 async def set_statut_channel(ctx):
@@ -85,7 +88,20 @@ pour le serveur {ctx.guild.name} (id : {ctx.guild.id}) ')
 async def helplease(ctx):
     await ctx.send(help_message)
 
-
+@bot.command()
+async def game_info(ctx, summoner_name, region='euw'):
+    #dict account
+    await dict_build(f"https://{all_my_dicts['region'][region]}.api.riotgames.com/lol/summoner/{api_version_riot}/summoners/by-name/{summoner_name}?api_key={api_key_riot}",'profile_{summoner_name}')
+    #dict spectator
+    await dict_build(f"https://{all_my_dicts['region'][region]}.api.riotgames.com/lol/spectator/{api_version_riot}/active-games/by-summoner/{all_my_dicts['profile_{summoner_name}']['id']}?api_key={api_key_riot}",'spectator')
+    print(f"https://{all_my_dicts['region'][region]}.api.riotgames.com/lol/spectator/{api_version_riot}/active-games/by-summoner/{all_my_dicts['profile_{summoner_name}']['id']}?api_key={api_key_riot}")
+    embed_menu = discord.Embed(title=f"List of summoners in {all_my_dicts['profile_{summoner_name}']['name']}'s game :")
+    
+    embed_menu.add_field(name='**red team**', value=f":one:{all_my_dicts['spectator']}\n:two:{s2}\n:three:{s3}\n:four:{s4}\n:five:{s5}'.format(s1=summoner0.Name(0),s2=summoner0.Name(1),s3=summoner0.Name(2),s4=summoner0.Name(3),s5=summoner0.Name(4)),inline=True")
+    embed_menu.add_field(name='**blue team**',value=':six:{s6}\n:seven:{s7}\n:eight:{s8}\n:nine:{s9}\n:keycap_ten:{s10}'.format(s6=summoner0.Name(5),s7=summoner0.Name(6),s8=summoner0.Name(7),s9=summoner0.Name(8),s10=summoner0.Name(9)),inline=True)
+    embed_menu.set_footer(text="react to get more tips")
+    
+    await ctx.send(embed=embed_menu)
 #<===============================================================================================================================================>#
 
 bot.run(api_key_discord)
